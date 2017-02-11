c754d15f4e8c6d4303564fa4436efabc95f0fb0f
Michael Friedman <contact@michaeldavidfriedman.com>
2017-02-11 09:07:15 -0800

    Wrote shallow test for Table component with Enzyme.

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import App, { Search, Button, Table } from './App'
 
 it('renders without crashing', () => {
@@ -51,6 +52,12 @@ describe('Table', () => {
       { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' }
     ]
   }
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table {...props} />
+    )
+    expect(element.find('.table-row').length).toBe(2)
+  })
   it('renders', () => {
     const div = document.createElement('div')
     ReactDOM.render(<Table {...props} />, div)
